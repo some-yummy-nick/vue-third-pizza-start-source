1f1f9b995dcf215d53b21b5001e08fcc23fb8d52
some.yummy.nick <some.yummy.nick@gmail.com>
2023-07-10 21:24:16 +0300

    refactor: 3.15.рефакторинг стилей и компонентов

--- frontend/src/modules/constructor/IngredientsSelector.vue
+++ frontend/src/modules/constructor/IngredientsSelector.vue
@@ -10,27 +10,12 @@
         >
           <li class="ingredients__item">
             <span :class="'filling filling--' + item.value">{{ item.name }}</span>
-            <div class="counter counter--orange ingredients__counter">
-              <button type="button"
-                      class="counter__button counter__button--minus"
-                      :disabled="getValue(item.value) === 0"
-                      @click="decrementValue(item.value)"
-              >
-                <span class="visually-hidden">Меньше</span>
-              </button>
-              <input type="text" name="counter"
-                     class="counter__input"
-                     :value="getValue(item.value)"
-                     @input="inputValue(item.value, $event.target.value)"
-              >
-              <button type="button"
-                      class="counter__button counter__button--plus"
-                      :disabled="getValue(item.value) === MAX_INGREDIENT_COUNT"
-                      @click="incrementValue(item.value)"
-              >
-                <span class="visually-hidden">Больше</span>
-              </button>
-            </div>
+            <app-counter
+                :value="getValue(item.value)"
+                :min="0"
+                :max="MAX_INGREDIENT_COUNT"
+                @input="inputValue(item.value, $event)"
+            />
           </li>
         </app-drag>
       </template>
@@ -42,6 +27,8 @@
 import {toRef} from "vue";
 
 import AppDrag from '@/common/components/AppDrag.vue'
+import AppCounter from '@/common/components/AppCounter.vue'
+
 import {MAX_INGREDIENT_COUNT} from "@/common/constants";
 
 const props = defineProps({
@@ -65,17 +52,13 @@ const setValue = (ingredient, count) => {
   emit("update", ingredient, Number(count));
 };
 
-const decrementValue = (ingredient) => {
-  setValue(ingredient, getValue(ingredient) - 1);
-};
-
-const incrementValue = (ingredient) => {
-  setValue(ingredient, getValue(ingredient) + 1);
-};
-
 const inputValue = (ingredient, count) => {
   return setValue(ingredient, Math.min(MAX_INGREDIENT_COUNT, Number(count)));
 };
+
+const update = (ingredient, count) => {
+  emit("update", ingredient, Number(count));
+}
 
 const emit = defineEmits(["update"]);
 </script>
